e7b9bbd87d9e05050cd21a38973c11ea8f975498
Charlie <charlie@airjelly.ai>
2026-09-02 15:52:55 +0800

    perf: add cache headers and fix app text trailing period

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.less";
 
-const App: React.FC = () => <div>CAM 需要通过基座加载。</div>;
+const App: React.FC = () => <div>CAM 需要通过基座加载</div>;
 
 export default App;
